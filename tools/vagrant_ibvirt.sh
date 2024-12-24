cbbd5c928b2ed848d04bae95181a52fc582996f3
Roberto J Rojas <robertojrojas@gmail.com>
2024-12-24 13:45:29 -0500

    updates ruby-bundler install comments
    Signed-off-by: Roberto J Rojas <robertojrojas@gmail.com>

--- tools/vagrant_ibvirt.sh
+++ tools/vagrant_ibvirt.sh
@@ -7,10 +7,9 @@
 #  I added this shell script to my $PATH so I can run it from any directory.
 #
 # Arch Linux:
-#  - sudo pacman -S ruby
+#  - sudo pacman -S ruby ruby-bundler
 #  - Install yay (https://github.com/Jguer/yay?tab=readme-ov-file#installation)
 #  - yay -S ruby-excon
-#  - gem install bundler # installs it locally so add ${HOME}/.local/share/gem/ruby/3.3.0/bin to $PATH
 #
 # The following is a modified version of the Development instructions from 
 #   https://github.com/robertojrojas/vagrant-libvirt?tab=readme-ov-file#development
@@ -23,8 +22,6 @@
 # bundle config --local path 'vendor/bundle'
 # bundle install
 
-# In case your Bundler gem is installed locally
-# export PATH=${HOME}/.local/share/gem/ruby/3.3.0/bin:$PATH
 export VAGRANT_VERSION=${VAGRANT_VERSION:-v2.4.3}
 export VAGRANT_DEFAULT_PROVIDER=libvirt
 export VAGRANT_CWD=${PWD}
